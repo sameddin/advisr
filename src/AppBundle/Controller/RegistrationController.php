7ad22649a538455f087cb0965286eed4c2f7cc4d
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-12-01 02:18:04 +0400

    Add User registration form

---
+++ src/AppBundle/Controller/RegistrationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\Type\UserType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/register", name="registration")
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(new UserType(), $user);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render(
+            '@App/registration/registration.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+}

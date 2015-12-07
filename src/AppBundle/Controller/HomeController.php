89062c74c31d0f7045fe704ee22b3b3af6fb1654
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-12-08 02:50:51 +0400

    Use @Template instead of hardcoding template to render

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -1,17 +1,21 @@
 <?php
-
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * @Route("/")
+ */
 class HomeController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route(name="homepage")
+     * @Template
      */
     public function homeAction()
     {
-        return $this->render('home/home.html.twig');
+        return [];
     }
 }

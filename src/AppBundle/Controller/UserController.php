4b20d54a50b784525115f35733121111e6bcaa16
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-12-09 03:10:04 +0400

    Add pagination to the list of users

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -1,11 +1,14 @@
 <?php
-
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
 use Doctrine\ORM\EntityManager;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/users", service="app.user_controller")
@@ -18,25 +21,42 @@ class UserController
     private $em;
 
     /**
-     * @param EntityManager $em
+     * @var PaginatorInterface
      */
-    public function __construct(EntityManager $em)
+    private $paginator;
+
+    /**
+     * @param EntityManager $em
+     * @param PaginatorInterface $paginator
+     */
+    public function __construct(EntityManager $em, PaginatorInterface $paginator)
     {
         $this->em = $em;
+        $this->paginator = $paginator;
     }
 
     /**
      * @Route(name="user.list")
      * @Template
+     *
+     * @param Request $request
+     * @return RedirectResponse|Response
      */
-    public function listAction()
+    public function listAction(Request $request)
     {
         $users = $this->em
             ->getRepository('AppBundle:User')
             ->findAll();
 
+        $pagination = $this->paginator->paginate(
+            $users,
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return [
-            'users' => $users
+            'users' => $users,
+            'pagination' => $pagination,
         ];
     }
 

5a77d0914e977d49635f4f6f6011f372050903de
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-12-21 02:24:50 +0400

    Globally disable HTML5 form validation

---
+++ src/App/Form/Extension/NoValidateExtension.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+class NoValidateExtension extends AbstractTypeExtension
+{
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['attr'] = array_merge($view->vars['attr'], [
+            'novalidate' => 'novalidate',
+        ]);
+    }
+
+    public function getExtendedType()
+    {
+        return FormType::class;
+    }
+}

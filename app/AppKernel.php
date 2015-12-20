fb08eee50965927b76061664b20928eaf1588bb8
Elnur Abdurrakhimov <elnur@elnur.pro>
2015-12-20 15:47:34 +0300

    Get rid of the app bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Elnur\DatabaseBundle\ElnurDatabaseBundle(),
             new Ornicar\GravatarBundle\OrnicarGravatarBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),

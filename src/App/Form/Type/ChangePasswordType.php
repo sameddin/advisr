aaabf1d5fcbcb2655d7bd719127c83c2e26509fa
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-12-22 02:59:13 +0400

    Require a user to provide their current password to change it

--- src/App/Form/Type/ChangePasswordType.php
+++ src/App/Form/Type/ChangePasswordType.php
@@ -6,19 +6,25 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 class ChangePasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('oldPassword', PasswordType::class, [
+                'label' => 'user.password.current',
+                'mapped' => false,
+                'constraints' => new UserPassword(),
+            ])
             ->add('raw_password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => ['label' => 'new.password'],
                 'second_options' => ['label' => 'new.password.confirm'],
             ])
             ->add('save', SubmitType::class, [
-                'label' => 'common.save'
+                'label' => 'common.save',
             ]);
     }
 

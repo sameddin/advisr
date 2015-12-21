36aaf0620d62989b7293f06a344baacca8de7710
Elnur Abdurrakhimov <elnur@elnur.pro>
2015-12-21 15:13:21 +0300

    Move the logs directory to the var/ directory

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -40,6 +40,11 @@ class AppKernel extends Kernel
         return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
     }
 
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
